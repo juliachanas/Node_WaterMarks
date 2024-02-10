884cbf3bc8245cc2024c3b8566a7eb4a5947e9a6
Julia Chanas <juulia.chanas@gmail.com>
2024-02-10 21:24:26 +0100

    Center watermark

--- app.js
+++ app.js
@@ -18,3 +18,26 @@ addTextWatermarkToImage(
   './test-with-watermark.jpg',
   'Hello world'
 );
+
+const addImageWatermarkToImage = async function (
+  inputFile,
+  outputFile,
+  watermarkFile
+) {
+  const image = await Jimp.read(inputFile);
+  const watermark = await Jimp.read(watermarkFile);
+  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+
+  image.composite(watermark, x, y, {
+    mode: Jimp.BLEND_SOURCE_OVER,
+    opacitySource: 0.5,
+  });
+  await image.quality(100).writeAsync(outputFile);
+};
+
+addImageWatermarkToImage(
+  './test.jpg',
+  './test-with-watermark2.jpg',
+  './logo.png'
+);
